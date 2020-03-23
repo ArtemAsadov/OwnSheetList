dbe4a0c6769a72f7bff566e3f669acbeae932f33
Артем Асадов <kekc33rus@gmail.com>
2020-03-23 19:58:22 +0300

    Еще разок

---
+++ OwnSheetList/OwnSheetList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OwnSheetList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
