6a1c76a94bafbb4f3b80cdf86604afa8f32c020c
Артем Асадов <kekc33rus@gmail.com>
2020-03-23 21:05:44 +0300

    Создание элемента очереди

---
+++ OwnSheetList/OwnSheetList/MyImplList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OwnSheetList
+{
+    public class MyImplList<T> : List<T>
+    {
+        private Node<T> node;
+        private int size = 0;
+
+        public MyImplList()
+        {
+            this.node = new Node<T>();
+        }
+
+        public int Size() => 0;
+        public bool IsEmpty() => false;
+        public bool Contains(object o) => false;
+        public IEnumerator<T> Iterator() => null;
+        public new object[] ToArray() => new object[0];
+        public T[] ToArray(T[] a) => null;
+        public new bool Add(T t) => false;
+        public bool Remove(object o) => false; 
+        //public bool ContainsAll(Predicate<?> c)=>
+
+    }
+}
---
+++ OwnSheetList/OwnSheetList/Node.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OwnSheetList
+{
+    /// <summary>
+    /// Элемента очереди
+    /// </summary>
+    /// <param>Node next – тут мы будем хранить адрес следующего элемента коллекции;</param>
+    /// <param>Node previous – тут мы будем хранить адрес предыдущего элемента коллекции;</param>
+    /// <param>T t – это наш item;</param>
+    /// <param>int index – тут индекс текущего элемента коллекции.</param>
+    public class Node<T>
+    {
+        private Node<T> next;
+        private Node<T> previous;
+        private T t;
+        public Node()
+        {
+            this.next = null;
+            this.previous = null;
+        }
+
+        public Node(Node<T> previous)
+        {
+            this.next = null;
+            this.previous = previous;
+        }
+
+        public Node<T> GetNext() => next;
+        public void SetNext(Node<T> next)=> this.next = next;
+        public Node<T> GetPrevious()=> previous;
+        public void SetPrevious(Node<T> previous)=> this.previous = previous;
+        public T GetT() => t;
+        public void SetT(T t) => this.t = t;
+    }
+}
